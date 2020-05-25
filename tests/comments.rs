545a65c8d43bff9ea953dc371543709209dd0487
Mathias Iversen <work@mathiasiversen.com>
2020-05-25 14:27:55 +0200

    added support for comments
    this commit adds the possibility to comment code, these comments are
    then skipped when parsed

---
+++ tests/comments.rs
@@ -0,0 +1,22 @@
+use data_parser::{Data, Result};
+use indoc::indoc;
+use insta::assert_json_snapshot;
+
+#[test]
+fn it_can_parse_object_with_comments() -> Result<()> {
+    let markup = indoc!(
+        r##"{
+            # This is a comment
+            "values": {
+                "r": "10",
+                "g": "10",
+                "b": "100" # this is also a comment
+            },
+            "two": "rgba(${values.r}, ${values.g}, ${values.b}, 0.1)",
+            "tree": "${values.r}px"
+        }"##
+    );
+    let x = Data::parse(markup)?;
+    assert_json_snapshot!(x);
+    Ok(())
+}
